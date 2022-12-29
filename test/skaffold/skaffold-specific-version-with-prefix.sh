0e4ebead8c7d7c08dd9ea5239e985b6a3d2ca678
Rio Kierkels <riokierkels@gmail.com>
2022-12-29 09:45:52 +0000

    feat(skaffold): allow for dynamic versions

--- test/skaffold/skaffold-specific-version-with-prefix.sh
+++ test/skaffold/skaffold-specific-version-with-prefix.sh
@@ -7,10 +7,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "skaffold is installed" skaffold version --output='skaffold version {{.Version}}, config version {{.ConfigVersion}}, commit {{.GitCommit}}, built at {{.BuildDate}} using {{.GoVersion}}'
-
-# Check version output
-check "skaffold is installed" skaffold version
+check "skaffold specific version v2.0.3" /bin/bash -c "skaffold version | grep 'v2.0.3'"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.

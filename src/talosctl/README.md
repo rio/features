bd227716aeb11739bf11e295f90b4f109c47b78e
Kevin Tijssen <kevin.tijssen@gmail.com>
2025-02-07 14:17:20 +0100

    Updating files: 8 (#3)
    * Updating files: 8
    
    * Updating files: 1
    
    * Updating files: 2

---
+++ src/talosctl/README.md
@@ -0,0 +1,24 @@
+
+# talosctl (talosctl)
+
+`talosctl` is the command-line tool used to manage and interact with Talos Linux, a secure, minimal, and immutable operating system designed for Kubernetes. It allows you to perform administrative tasks such as bootstrapping a cluster, configuring nodes, and debugging issues.
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/ktijssen/features/talosctl:1": {}
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+| version | Version of gum to install. Accepts versions with or without the 'v' prefix. | string | latest |
+
+
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](https://github.com/ktijssen/features/blob/main/src/talosctl/devcontainer-feature.json).  Add additional notes to a `NOTES.md`._
---
+++ src/talosctl/install.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+set -e
+
+# grab the version
+readonly TALOSCTL_VERSION="${VERSION:-latest}"
+
+# apt-get configuration
+export DEBIAN_FRONTEND=noninteractive
+
+
+preflight () {
+    if command -v wget > /dev/null; then
+        return
+    fi
+
+    if [ -e /etc/os-release ]; then
+        . /etc/os-release
+    fi
+
+    case "${ID}" in
+        'debian' | 'ubuntu')
+            apt-get update
+            apt-get install -y --no-install-recommends \
+                wget \
+                ca-certificates
+        ;;
+        'fedora')
+            dnf -y install wget
+        ;;
+        *) echo "The ${ID} distribution is not supported."; exit 1 ;;
+    esac
+}
+
+main () {
+    preflight
+
+    local ARCH="$(uname -m)"
+    case "${ARCH}" in
+        "aarch64") ARCH="arm64" ;;
+        "x86_64") ARCH="amd64" ;;
+        *) echo "The current architecture (${ARCH}) is not supported."; exit 1 ;;
+    esac
+
+    local TALOSCTL_URL="https://github.com/siderolabs/talos/releases/latest/download/talosctl-linux-${ARCH}"
+    if [ "${TALOSCTL_VERSION}" != "latest" ] ; then
+        TALOSCTL_URL="https://github.com/siderolabs/talos/releases/download/v${TALOSCTL_VERSION#[vV]}/talosctl-linux-${ARCH}"
+    fi
+
+    echo "Installing talosctl ${TALOSCTL_VERSION} for ${ARCH} ..."
+
+    echo "Downloading ${TALOSCTL_URL} ..."
+    wget --no-verbose -O /usr/local/bin/talosctl "${TALOSCTL_URL}"
+    chmod +x /usr/local/bin/talosctl
+
+    echo "talosctl ${TALOSCTL_VERSION} for ${ARCH} installed at $(command -v talosctl) with checksum $(sha256sum $(command -v talosctl))."
+}
+
+main "$@"

056e6e810a2deb5f519a10b15ea221d70b888347
Rio Kierkels <riokierkels@gmail.com>
2025-10-21 09:30:25 +0000

    feat: chew bubble gum
    fixes #52

--- test/gum/test.sh
+++ test/gum/test.sh
@@ -7,13 +7,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "skaffold" skaffold version
-check "chezmoi" chezmoi --version
 check "gum" gum --version
-check "kustomize" kustomize version
-check "k9s" k9s version
-check "k3d" k3d version
-check "vcluster" vcluster version
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
